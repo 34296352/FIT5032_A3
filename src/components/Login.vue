a9345a90b86366124b881891163f614400a5b783
lijiawei <jlii0456@student.monash.edu>
2025-10-15 15:48:54 +1100

    finish the login page

---
+++ src/components/Login.vue
@@ -0,0 +1,32 @@
+<template>
+    <div class="form">
+    <h1>Login Page</h1>
+        <form>
+            <label for="email">Email:</label><br>
+            <input type="text" id="email" name="email"><br>
+            
+            <label for="password">Password:</label><br>
+            <input type="password" id="password" name="password"><br>
+            
+        </form>
+    </div>
+
+    <div class ="login-box">
+    <p>
+      <button @click="goToLogin">login</button>
+    </p>
+    </div>
+
+    <div class="register-box">
+    <p>
+      Haven't Email?
+      <button @click="goToRegister">click to register</button>
+    </p>
+    </div>
+
+    <div class="forget-password-box">
+      Forget password?
+      <button @click="goToFix">click to fix</button>
+    </div>
+</template>
+
